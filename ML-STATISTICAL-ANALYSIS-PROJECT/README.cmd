ba7ec51392bdf308bcbb2a1419b2cd1a72fad7ae
Katia Hernández <104922109+Katiahsotelo@users.noreply.github.com>
2022-11-19 14:08:45 -0600

    Update README.cmd

--- ML-STATISTICAL-ANALYSIS-PROJECT/README.cmd
+++ ML-STATISTICAL-ANALYSIS-PROJECT/README.cmd
@@ -3,9 +3,14 @@
 
 ## Project Description
 
-This project uses linear and machine leraning models to predict housing sale prices in Boston, Massachusetts. 
+This project uses linear and machine learning models to predict housing sale prices in Boston, Massachusetts. 
 
-I selected the most important  features from a total of 397 to create predictions using Machine Learning 
+The housing sales prices depends of the house features, the materials, the neighborhood, area, etc. In this project i chose the most important features to predict the  house prices in this area of the U.S using several models 
+
+at the end i present a resume of the accuarancy of each model and the standard deviation to know wich is the best model used
+
+
+
 ## Variables used
 
  Years 2012, 2014 and 2016
