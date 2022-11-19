30d23bd4f34782d1f1731f2c210a1070a0f7b3e5
Katia Hernández <104922109+Katiahsotelo@users.noreply.github.com>
2022-11-19 00:12:59 -0600

    Update README.cmd

--- ML-STATISTICAL-ANALYSIS-PROJECT/README.cmd
+++ ML-STATISTICAL-ANALYSIS-PROJECT/README.cmd
@@ -1,1 +1,39 @@
 
+# Predictions of housing sales prices
+
+## Project Description
+
+This project uses linear and machine leraning models to predict housing sale prices in Boston, Massachusetts. 
+
+I selected the most important  features from a total of 397 to create predictions using Machine Learning 
+## Variables used
+
+ Years 2012, 2014 and 2016
+
+- Canopy density 
+- Canopy base height 
+- Canopy cover 
+- Canpy height 
+- Elevation of vegetation
+- Vegetation height 
+- Type of vegetation
+
+
+ Year 2016 
+
+- Polygon elevation 
+- Height of burned vegetation
+- Type of burned vegetation 
+- Inclination of the polygon
+
+
+ Year 2012 to 2018
+
+- Number of daily wildfires per polygon 
+- Radiative power of the fire
+- Number of neighboring polygons set on fire daily
+
+
+## Link to the presentation 
+
+https://www.canva.com/design/DAFOTE5JLDY/uuiVnSriSAw8fr6APXyGrw/view?utm_content=DAFOTE5JLDY&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton
